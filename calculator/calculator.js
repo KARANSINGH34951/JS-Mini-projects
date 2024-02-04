65d5eb4f5cefcd6e4b6a2559567c5ef7322ca289
karan singh D <ceitkaransingh2026@gmail.com>
2024-02-04 13:11:39 +0530

    added js file

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,6 +3,46 @@ let buttons= document.querySelectorAll("button")
 
 let string=""
 
+let arr = Array.from(buttons);
+
+arr.forEach(buttons=>{
+  buttons.addEventListener("click",(e)=>{
+    if(e.target.innerHTML=="="){
+      string=eval(string);
+      displaybox.value=string;
+    }
+    else if(e.target.innerHTML=="AC"){
+      string="";
+      displaybox.value=string
+    }
+    else if(e.target.innerHTML=="DEL"){
+      string=string.substring(0,string.length-1);
+      displaybox.value=string
+    }
+    else{
+      string+=e.target.innerHTML
+      displaybox.value=string
+    }
+
+  })
+})
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
 
 
 
