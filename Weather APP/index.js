55097f8c211ac0a587d80bd7c33cc21e99594276
karan singh D <ceitkaransingh2026@gmail.com>
2024-02-10 21:32:50 +0530

    weather app added

---
+++ Weather APP/index.js
@@ -0,0 +1,14 @@
+const url = 'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=%3Cspan%20class=e883c4b754eb3f40ffa1ed8b60fb1d5a'
+;
+
+async function fetchData() {
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+fetchData();
